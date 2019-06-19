b9db47060f784ffadfa8a987a57c872d0a7586c5
Rome Bell <romebell@me.com>
2019-06-19 10:16:36 -0700

    Create logger.js and add logger and id to demostrate use case

--- 2019/W24D5/node-crash-course/index.js
+++ 2019/W24D5/node-crash-course/index.js
@@ -11,3 +11,14 @@ person1.greeting();
 
 // console.log(person1.greeting);
 // console.log(person2.greeting());
+
+console.log('============== Logger ==============');
+const Logger = require('./logger');
+
+const logger = new Logger();
+
+logger.on('message', (data) => console.log('Called listener', data));
+
+logger.log('Hello world');
+logger.log('Hi, Rome');
+logger.log('Hello');
