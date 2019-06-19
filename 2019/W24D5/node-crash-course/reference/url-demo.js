985931afb1720bd7559845e0bb277e9a391ca533
Rome Bell <romebell@me.com>
2019-06-18 23:04:16 -0700

    Add url-demo.js and add url modules examples

---
+++ 2019/W24D5/node-crash-course/reference/url-demo.js
@@ -0,0 +1,34 @@
+// URL modules
+const URL = require('url').URL;
+// console.log(url);
+
+const myUrl = new URL('http://mywebsite.com/hello.html?id=100&status=active'); 
+
+// Serialized URL
+console.log(myUrl.href);
+console.log(myUrl.toString());
+
+// Host (root domain)
+console.log(myUrl.host);
+
+// Hostname does not get port
+console.log(myUrl.hostname);
+
+// Path name
+console.log(myUrl.pathname);
+
+// Serialized Query
+console.log(myUrl.search);
+
+// Params object
+console.log(myUrl.searchParams);
+
+// Add params
+myUrl.searchParams.append('abc', '123');
+console.log(myUrl.searchParams);
+
+// Loop through params
+myUrl.searchParams.forEach((value, name) => console.log(`${name}: ${value}` ));
+
+// var a = new URL("/", "https://developer.mozilla.org");
+// console.log(a.href);
