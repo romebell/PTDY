4654cf5a49a846461a9e3c45dea8d60585c343af
Rome Bell <romebell@me.com>
2019-07-10 23:04:30 -0700

    Add scraper.py to sextract amazon website data

---
+++ 2019/W28D3/amazon-scraper/scraper.py
@@ -0,0 +1,12 @@
+import requests
+from bs4 import BeautifulSoup
+
+URL = 'https://www.amazon.com/Learning-Python-5th-Mark-Lutz/dp/1449355730/ref=sr_1_3?keywords=python&qid=1562823199&s=gateway&sr=8-3'
+
+headers = { "User-Agent": 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/75.0.3770.100 Safari/537.36'}
+
+page = requests.get(URL, headers=headers)
+
+soup = BeautifulSoup(page.content, 'html.parser')
+
+print(soup.prettify())
