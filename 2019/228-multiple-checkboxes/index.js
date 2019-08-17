2c9378bfebd7e4e659d803dd4ec57e4f4d56fd08
Rome Bell <romebell@me.com>
2019-08-17 12:33:46 -0700

    Add functionality to checkboxes

---
+++ 2019/228-multiple-checkboxes/index.js
@@ -0,0 +1,30 @@
+const checkboxes = document.querySelectorAll('.inbox [type="checkbox"]');
+console.log(checkboxes);
+
+let lastChecked;
+
+function handleCheck(e) {
+  console.log(e);
+  // Check if they the shift key down
+  // AND check that they are checking it
+  let inBetween = false;
+  if (e.shiftKey && this.checked) {
+    // go ahead and do what we want
+    // loop over every single checkbox
+    checkboxes.forEach(checkbox => {
+      console.log(checkbox);
+      if (checkbox === this || checkbox == lastChecked) {
+        inBetween = !inBetween;
+        console.log('starting to check in between')
+      }
+
+      if (inBetween) {
+        checkbox.checked = true;
+      }
+    })
+  }
+  lastChecked = this;
+}
+
+checkboxes.forEach(checkbox => checkbox.addEventListener(
+  'click', handleCheck));
